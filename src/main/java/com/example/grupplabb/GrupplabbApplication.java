29001a7d8db2ab8fa61a5ba64c6e88892f5f2658
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-02-05 10:06:36 +0100

    first commit

---
+++ src/main/java/com/example/grupplabb/GrupplabbApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupplabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupplabbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupplabbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupplabb/GrupplabbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupplabb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupplabbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
