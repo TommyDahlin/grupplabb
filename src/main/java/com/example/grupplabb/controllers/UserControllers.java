dd637f1e1ce6cce01b8601c91440440ea2607bc0
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-02-05 11:12:57 +0100

    final touches and tested user with postman

--- src/main/java/com/example/grupplabb/controllers/UserControllers.java
+++ src/main/java/com/example/grupplabb/controllers/UserControllers.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping(value = "api/")
+@RequestMapping(value = "/api")
 public class UserControllers {
     @Autowired
     UserService userService;
--- src/main/java/com/example/grupplabb/models/User.java
+++ src/main/java/com/example/grupplabb/models/User.java
@@ -24,4 +24,7 @@ public class User {
     public String getEmail() {
         return email;
     }
+    public String getPassword(){
+        return password;
+    }
 }
